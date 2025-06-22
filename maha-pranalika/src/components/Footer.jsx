464478bd77eca9f986ca6bbb2de6c51bfc6e63ec
mekalavishleshana <20211a05h6@bvrit.ac.in>
2025-06-22 14:22:31 +0530

    Added footer

--- maha-pranalika/src/components/Footer.jsx
+++ maha-pranalika/src/components/Footer.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import './Footer.css';
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+    <div className="footer-address">
+      flat no 101, MIG 416, Road No. 4, Kukatpally Housing Board Colony,<br />
+      Kukatpally, Hyderabad, Telangana 500072
+    </div>
+
+    <div className="footer-map">
+      <iframe
+        title="Google Map"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.3215421290466!2d78.39177231487649!3d17.442433888049112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb93d24a3d295f%3A0xc5ad3f19f63a3a8b!2sKukatpally%2C%20Hyderabad%2C%20Telangana%20500072!5e0!3m2!1sen!2sin!4v1710000000000"
+        width="100%"
+        height="200"
+      ></iframe>
+    </div>
+
+    <div className="footer-contact">
+      📞 +91 789-388-1269 &nbsp; | &nbsp; 📧 info@mahapranalika.com
+    </div>
+  </footer>
   )
 }
