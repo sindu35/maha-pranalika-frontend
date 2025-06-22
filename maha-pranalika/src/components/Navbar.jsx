4b1946c0d650efdd48b189be94b7b456459c9bac
Gubbala Sai Nikhil <gsn.agc5022@gmail.com>
2025-06-22 14:00:40 +0530

    chore: Setting up basic navigation items

--- maha-pranalika/src/components/Navbar.jsx
+++ maha-pranalika/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import '../styles/navbar.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="nav">
+      <div>Home</div>
+      <div>Services</div>
+      <div>Privacy Policy</div>
+      <div>FAQ</div>
+      <div>Terms & conditions</div>
+      <div onClick={() => navigate('/signup')}>Sign Up</div>
+    </div>
+  );
 }
--- maha-pranalika/src/routes/AppRoutes.jsx
+++ maha-pranalika/src/routes/AppRoutes.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
+import Signup from '../pages/Signup'
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
-    </BrowserRouter>
   )
 }
 
